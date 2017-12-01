0883bcf3ad198693f1d00c8abe96e532f0379a9f
pankumazumdar <pankumazumdar@example.com>
2017-12-01 13:14:57 +0530

    Complete rest of website

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,25 +2,34 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
 
-    browserSync.init({
-        notify: false,
-        server: {
-            baseDir: "app"
-        }
-    });
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
 
-    watch('./app/index.html', function () {
-        browserSync.reload();
-    });
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
 
-    watch('./app/assets/styles/**/*.css', function () {
-        gulp.start('cssInject');
-    });
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
+
 });
 
-gulp.task('cssInject', ['styles'], function () {
-    return gulp.src('./app/temp/styles/styles.css')
-        .pipe(browserSync.stream());
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
